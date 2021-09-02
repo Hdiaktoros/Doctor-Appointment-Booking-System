f64133d6798b4d472af7230de6ed76dd5d97899c
Hdiaktoros <frimpongasante50@gmail.com>
2021-09-02 15:06:36 +0000

    cj

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,15 +1,18 @@
 import db from "../models";
 
 let createNewUser = (user) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+           
 
-	return new Promise(async (resolve, reject) => {
-		try {
-			await db.User.create(user);
-			resolve("done!")
-		} catch (e) {
-			reject(e);
-		}
-	});
+                //create a new user
+                await db.User.create(user);
+                resolve("done!")
+            
+        } catch (e) {
+            reject(e);
+        }
+    });
 };
 
 module.exports = {
