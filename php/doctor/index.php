b4b4f25b237e7e9d0a09ebdc8a3e9efe53209e51
Hdiaktoros <frimpongasante50@gmail.com>
2021-09-15 21:01:32 +0000

    l

--- php/doctor/index.php
+++ php/doctor/index.php
@@ -1,91 +1,191 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-//index.php
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Xpert Health - Login</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Advent+Pro">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alfa+Slab+One">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
 
-include('class/Appointment.php');
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="57"><nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container"><a class="navbar-brand" href="index.php#page-top">Xpert health</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-align-justify"></i></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="#home">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+                <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                <?php
+		    if(!isset($_SESSION['patient_id']))
+		    {
+		    ?>
+                <li class="nav-item"><a class="nav-link" href="login.php">login</a></li>
+                <li class="nav-item"><a class="nav-link" href="register.php" style="transform: skew(0deg);">register</a></li>
+                <?php
+                }
+                else
+                {
+                ?>
+                <li class="nav-item"><a class="nav-link" href="client/dashboard.php">Dashboard</a></li>
+                <?php
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</nav><header id="home"class="text-center text-white d-flex masthead" style="background-image:url('assets/img/header.jpg');">
+<div class="content">
+			<h1> Book for Consulting <br> today and get a kit </h1>
+			<div class="search-box">
+				<input type="text" placeholder="Book for Consulting, Check-ups and more...">
+				<div class="autocom-box">
+        		</div>
+                <button type="submit"><img src="assets/img/racks1.png" alt="search"></button>
+			</div>
+			<p> A wide range of Doctors, Physicians and more at your call </p>
+		</div>
+				</header>
+    <section id="about" class="bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col offset-lg-8 text-center mx-auto">
+                    <h2 class="text-white section-heading">We've got what you need!</h2>
+                    <hr class="light my-4">
+                    <p class="text-faded mb-4">Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes are open source, free to download, and easy to use. No strings attached!</p><a class="btn btn-light btn-xl" role="button" href="#services">Get Started!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">At Your Service</h2>
+                    <hr class="my-4">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="mx-auto service-box mt-5"><i class="fa fa-diamond fa-4x text-primary mb-3 sr-icons" data-aos="zoom-in" data-aos-duration="200" data-aos-once="true"></i>
+                        <h3 class="mb-3">Sturdy Templates</h3>
+                        <p class="text-muted mb-0">Our templates are updated regularly so they don't break.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="mx-auto service-box mt-5"><i class="fa fa-paper-plane fa-4x text-primary mb-3 sr-icons" data-aos="zoom-in" data-aos-duration="200" data-aos-delay="200" data-aos-once="true"></i>
+                        <h3 class="mb-3">Ready to Ship</h3>
+                        <p class="text-muted mb-0">You can use this theme as is, or you can make changes!</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="mx-auto service-box mt-5"><i class="fa fa-newspaper-o fa-4x text-primary mb-3 sr-icons" data-aos="zoom-in" data-aos-duration="200" data-aos-delay="400" data-aos-once="true"></i>
+                        <h3 class="mb-3">Up to Date</h3>
+                        <p class="text-muted mb-0">We update dependencies to keep things fresh.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 text-center">
+                    <div class="mx-auto service-box mt-5"><i class="fa fa-heart fa-4x text-primary mb-3 sr-icons" data-aos="fade" data-aos-duration="200" data-aos-delay="600" data-aos-once="true"></i>
+                        <h3 class="mb-3">Made with Love</h3>
+                        <p class="text-muted mb-0">You have to make your websites with love these days!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="portfolio" class="p-0">
+        <div class="container-fluid p-0">
+            <div class="row g-0 popup-gallery">
+                <div class="col-sm-6 col-lg-4"><a class="portfolio-box" href="assets/img/fullsize/1.jpg"><img class="img-fluid" src="assets/img/thumbnails/1.jpg">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded"><span>Category</span></div>
+                                <div class="project-name"><span>Project Name</span></div>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col-sm-6 col-lg-4"><a class="portfolio-box" href="assets/img/fullsize/2.jpg"><img class="img-fluid" src="assets/img/thumbnails/2.jpg">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded"><span>Category</span></div>
+                                <div class="project-name"><span>Project Name</span></div>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col-sm-6 col-lg-4"><a class="portfolio-box" href="assets/img/fullsize/3.jpg"><img class="img-fluid" src="assets/img/thumbnails/3.jpg">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded"><span>Category</span></div>
+                                <div class="project-name"><span>Project Name</span></div>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col-sm-6 col-lg-4"><a class="portfolio-box" href="assets/img/fullsize/4.jpg"><img class="img-fluid" src="assets/img/thumbnails/4.jpg">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded"><span>Category</span></div>
+                                <div class="project-name"><span>Project Name</span></div>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col-sm-6 col-lg-4"><a class="portfolio-box" href="assets/img/fullsize/5.jpg"><img class="img-fluid" src="assets/img/thumbnails/5.jpg">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded"><span>Category</span></div>
+                                <div class="project-name"><span>Project Name</span></div>
+                            </div>
+                        </div>
+                    </a></div>
+                <div class="col-sm-6 col-lg-4"><a class="portfolio-box" href="assets/img/fullsize/6.jpg"><img class="img-fluid" src="assets/img/thumbnails/6.jpg">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded"><span>Category</span></div>
+                                <div class="project-name"><span>Project Name</span></div>
+                            </div>
+                        </div>
+                    </a></div>
+            </div>
+        </div>
+    </section>
+    <section class="text-white bg-dark">
+        <div class="container text-center">
+            <h2 class="mb-4">Lorem Ipsum!</h2><a class="btn btn-light btn-xl sr-button" role="button" data-aos="zoom-in" data-aos-duration="400" data-aos-once="true" href="#">Download Now!</a>
+        </div>
+    </section>
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 text-center mx-auto">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="my-4">
+                    <p class="mb-5">Ready to start your next project with us? That's great! Give us a call or send us an email and we will get back to you as soon as possible!</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 text-center ms-auto"><i class="fa fa-phone fa-3x mb-3 sr-contact" data-aos="zoom-in" data-aos-duration="300" data-aos-once="true"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center me-auto"><i class="fa fa-envelope-o fa-3x mb-3 sr-contact" data-aos="zoom-in" data-aos-duration="300" data-aos-delay="300" data-aos-once="true"></i>
+                    <p><a href="mailto:your-email@your-domain.com">email@example.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
 
-$object = new Appointment;
-
-if(isset($_SESSION['patient_id']))
-{
-	header('location:dashboard.php');
-}
-
-$object->query = "
-SELECT * FROM doctor_schedule_table 
-INNER JOIN doctor_table 
-ON doctor_table.doctor_id = doctor_schedule_table.doctor_id
-WHERE doctor_schedule_table.doctor_schedule_date >= '".date('Y-m-d')."' 
-AND doctor_schedule_table.doctor_schedule_status = 'Active' 
-AND doctor_table.doctor_status = 'Active' 
-ORDER BY doctor_schedule_table.doctor_schedule_date ASC
-";
-
-$result = $object->get_result();
-
-include('header.php');
-
-?>
-		      	<div class="card">
-		      		<form method="post" action="result.php">
-			      		<div class="card-header"><h3><b>Doctor Schedule List</b></h3></div>
-			      		<div class="card-body">
-		      				<div class="table-responsive">
-		      					<table class="table table-striped table-bordered">
-		      						<tr>
-		      							<th>Doctor Name</th>
-		      							<th>Education</th>
-		      							<th>Speciality</th>
-		      							<th>Appointment Date</th>
-		      							<th>Appointment Day</th>
-		      							<th>Available Time</th>
-		      							<th>Action</th>
-		      						</tr>
-		      						<?php
-		      						foreach($result as $row)
-		      						{
-		      							echo '
-		      							<tr>
-		      								<td>'.$row["doctor_name"].'</td>
-		      								<td>'.$row["doctor_degree"].'</td>
-		      								<td>'.$row["doctor_expert_in"].'</td>
-		      								<td>'.$row["doctor_schedule_date"].'</td>
-		      								<td>'.$row["doctor_schedule_day"].'</td>
-		      								<td>'.$row["doctor_schedule_start_time"].' - '.$row["doctor_schedule_end_time"].'</td>
-		      								<td><button type="button" name="get_appointment" class="btn btn-primary btn-sm get_appointment" data-id="'.$row["doctor_schedule_id"].'">Get Appointment</button></td>
-		      							</tr>
-		      							';
-		      						}
-		      						?>
-		      					</table>
-		      				</div>
-		      			</div>
-		      		</form>
-		      	</div>
-		    
-
-<?php
-
-include('footer.php');
-
-?>
-
-<script>
-
-$(document).ready(function(){
-	$(document).on('click', '.get_appointment', function(){
-		var action = 'check_login';
-		var doctor_schedule_id = $(this).data('id');
-		$.ajax({
-			url:"action.php",
-			method:"POST",
-			data:{action:action, doctor_schedule_id:doctor_schedule_id},
-			success:function(data)
-			{
-				window.location.href=data;
-			}
-		})
-	});
-});
-
-</script>
+</html>
--- php/doctor/login.php
+++ php/doctor/login.php
@@ -1,43 +1,89 @@
+
+<!DOCTYPE html>
+<html lang="en">
 <?php
-
 //login.php
-
-include('header.php');
-
 include('class/Appointment.php');
-
 $object = new Appointment;
-
 ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Xpert Health - Login</title>
+    <meta property="og:title" content="Login">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Advent+Pro">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alfa+Slab+One">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="vendor/bootstrap/bootstrap.min.css">
 
+</head>
+
+<body style="background-image:url('assets/img/header.jpg');">
+   <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
+    <div class="container"><a class="navbar-brand" href="index.php#page-top">Xpert health</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-align-justify"></i></button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="index.php#Home">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php#about">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php#services">Services</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php#portfolio">Portfolio</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php#contact">Contact</a></li>
+                <?php
+                if(!isset($_SESSION['patient_id']))
+                {
+                    ?>
+                <li class="nav-item"><a class="nav-link active" href="login.php">login</a></li>
+                <li class="nav-item"><a class="nav-link" href="register.php">register</a></li>
+                    <?php
+                }
+                else{
+                ?>
+
+                <li class="nav-item"><a class="nav-link" href="client/dashboard.php">Dashboard</a></li>
+                    <?php
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+<header>
+
+<div class="container-fluid">
 <div class="container">
 	<div class="row justify-content-md-center">
 		<div class="col col-md-4">
-			<?php
-			if(isset($_SESSION["success_message"]))
-			{
-				echo $_SESSION["success_message"];
-				unset($_SESSION["success_message"]);
-			}
-			?>
-			<span id="message"></span>
+		    <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>
+						<span id="message"></span>
 			<div class="card">
-				<div class="card-header">Login</div>
+				<div class="card-header" style="padding:.5rem 9.5rem;">Login</div>
 				<div class="card-body">
 					<form method="post" id="patient_login_form">
 						<div class="form-group">
 							<label>Patient Email Address</label>
-							<input type="text" name="patient_email_address" id="patient_email_address" class="form-control" required autofocus data-parsley-type="email" data-parsley-trigger="keyup" />
+							<input type="text" name="patient_email_address" id="patient_email_address" class="form-control" required="" autofocus="" data-parsley-type="email" data-parsley-trigger="keyup" spellcheck="false" data-ms-editor="true">
 						</div>
 						<div class="form-group">
 							<label>Patient Password</label>
-							<input type="password" name="patient_password" id="patient_password" class="form-control" required  data-parsley-trigger="keyup" />
+							<input type="password" name="patient_password" id="patient_password" class="form-control" required="" data-parsley-trigger="keyup">
 						</div>
 						<div class="form-group text-center">
-							<input type="hidden" name="action" value="patient_login" />
-							<input type="submit" name="patient_login_button" id="patient_login_button" class="btn btn-primary" value="Login" />
+							<input type="hidden" name="action" value="patient_login">
+							<input type="submit" name="patient_login_button" id="patient_login_button" class="btn btn-primary" value="Login" style="user-select: auto;">
 						</div>
-
 						<div class="form-group text-center">
 							<p><a href="register.php">Register</a></p>
 						</div>
@@ -47,28 +93,15 @@ $object = new Appointment;
 		</div>
 	</div>
 </div>
-
-<?php
-
-include('footer.php');
-
-?>
-
-
-<script>
-
+    </header>
+    <script>
 $(document).ready(function(){
-
 	$('#patient_login_form').parsley();
-
 	$('#patient_login_form').on('submit', function(event){
-
 		event.preventDefault();
-
 		if($('#patient_login_form').parsley().isValid())
 		{
 			$.ajax({
-
 				url:"action.php",
 				method:"POST",
 				data:$(this).serialize(),
@@ -92,11 +125,13 @@ $(document).ready(function(){
 				}
 			});
 		}
-
 	});
-
 });
-
-
-
 </script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
